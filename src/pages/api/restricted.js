2c617dfcc79d316b3f63e42ea09d1e135c01a634
leonel-acosta <leoacosta88@gmail.com>
2024-11-07 15:36:55 +0100

    auth handler

--- src/pages/api/restricted.js
+++ src/pages/api/restricted.js
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
 
-export default async (req, res) => {
+const handler = async (req, res) => {
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
@@ -16,3 +16,4 @@ export default async (req, res) => {
     });
   }
 };
+u;
