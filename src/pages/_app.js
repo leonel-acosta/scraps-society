43b81d831a7bf9c83b9d8243380d809f3b4bef5c
leonel-acosta <leoacosta88@gmail.com>
2024-11-08 14:25:40 +0100

    next auth set up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </Layout>
   );
 }
