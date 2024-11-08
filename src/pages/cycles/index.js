56327847f7f98c92de22b422de8c7716d6be5db8
leonel-acosta <leoacosta88@gmail.com>
2024-11-08 18:51:56 +0100

    render posts in list

--- src/pages/cycles/index.js
+++ src/pages/cycles/index.js
@@ -1,3 +1,33 @@
+import CycleCard from "@/components/CycleCard";
+import useSWR from "swr";
+import { useState, useEffect } from "react";
+
 export default function CyclesPage() {
-  return <p>Under Construction</p>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch("/api/posts");
+      const posts = await response.json();
+      console.log(posts);
+      setPosts(posts);
+    }
+
+    startFetching();
+  }, []);
+
+  return (
+    <>
+      <h1>CYCLES</h1>
+      <ul role="list">
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <CycleCard title={post.title} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
