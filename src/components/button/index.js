ed572c34174c4ef3b1304875c8de89082a06262f
leonel-acosta <leoacosta88@gmail.com>
2024-11-08 14:42:43 +0100

    change name convention for components

--- src/components/button/index.js
+++
@@ -1,5 +0,0 @@
-import styles from "./button.module.css";
-
-export default function Button() {
-  return <button>Button</button>;
-}
--- src/components/tag/index.js
+++
@@ -1,5 +0,0 @@
-import styles from "./tag.module.css";
-
-export default function Tag() {
-  return <div>Tag</div>;
-}
