dc82660071eac42efd481ad8cf3504a0a849d33e
leonel-acosta <leoacosta88@gmail.com>
2024-11-11 12:23:53 +0100

    add fonts | global styling | card styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,16 @@
 import "@/styles/globals.css";
+import localFont from "next/font/local";
+
+const chivo = localFont({
+  src: "./fonts/Chivo-VariableFont_wght.ttf",
+  variable: "--font-chivo",
+  weight: "100 900",
+});
+const chivoMono = localFont({
+  src: "./fonts/ChivoMono-VariableFont_wght.ttf",
+  variable: "--font-chivo-mono",
+  weight: "100 900",
+});
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
