f6780b75dc605bc5399b1a4aac7815ecbbcbc411
leonel-acosta <leoacosta88@gmail.com>
2024-11-18 14:14:30 +0100

    fix navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,14 @@ import styles from "./Header.module.css";
 import Button from "../Button";
 import SearchBar from "../SearchBar";
 import UserInfo from "../UserInfo";
+import Link from "next/link";
 
 export default function Header({ onChange, searchTerm }) {
   return (
     <div className="p-5 bg-secondary justify-between flex flex-row static top-0 w-full">
-      <h1>Header</h1>
+      <Link href="./">
+        <h1>SCRAPS SOCIETY</h1>
+      </Link>
       <SearchBar onChange={onChange} searchTerm={searchTerm} />
       <UserInfo />
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
       <div className="gap-2 flex flex-col justify-center text-center py-10">
         <Link href="/api/users">USERS API</Link>
         <Link href="/api/posts">posts API</Link>
-        <Link href="/cycles">CYCLES </Link>
-        <Link href="/cycles/create">CREATE CYCLE </Link>
+        <Link href="/posts">CYCLES </Link>
+        <Link href="/posts/create">CREATE CYCLE </Link>
         <Link href="/user/update-profile">USER UPDATE </Link>
       </div>
     </>
